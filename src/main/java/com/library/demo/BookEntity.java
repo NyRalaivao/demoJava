850cdf1d703176563fa16f85cd168e2ebb8d1388
Ny <ralaivaonylalaina@gmail.com>
2026-06-13 11:31:32 +0300

    configuratio pm.xml et debut dans Entity

--- src/main/java/com/library/demo/BookEntity.java
+++ src/main/java/com/library/demo/BookEntity.java
@@ -1,4 +1,21 @@
 package com.library.demo;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
 public class BookEntity {
+    Long id;
+    String name;
+    Integer pages;
+
+    public BookEntity(Long id, String name, Integer pages) {
+        this.id = id;
+        this.name = name;
+        this.pages = pages;
+    }
+
 }
